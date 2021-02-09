ef333c2b1e3f7d5279e140b2ba8b2f3eeee7c5cd
kompotkot <zenitsws@gmail.com>
2021-02-09 09:41:56 +0000

    Updated version number

--- bugout/__init__.py
+++ bugout/__init__.py
@@ -7,7 +7,7 @@ __description__ = "Python client library for Bugout API"
 
 __email__ = "engineering@bugout.dev"
 __license__ = "MIT"
-__version__ = "0.0.9"
+__version__ = "0.1.0"
 
 __all__ = (
     "__author__",
