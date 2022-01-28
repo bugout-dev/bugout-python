247309aa338fa473c7d4b5d8dc1a98a86495c532
Asher Foa <1268088+asherf@users.noreply.github.com>
2022-01-28 11:04:07 -0500

    Properly handle network errors (no response object)

--- bugout/calls.py
+++ bugout/calls.py
@@ -7,23 +7,26 @@ from .exceptions import BugoutResponseException, BugoutUnexpectedResponse
 
 
 def make_request(method: Method, url: str, **kwargs) -> Any:
-    response_body = None
     try:
-        r = requests.request(method.value, url=url, **kwargs)
-        r.raise_for_status()
-        response_body = r.json()
-    except requests.exceptions.RequestException as e:
-        exception_detail = r.json()
+        response = requests.request(method.value, url=url, **kwargs)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        response = err.response
+        if not err.response:
+            # Connection errors, timepouts, etc...
+            raise BugoutResponseException("Network error", detail=str(err))
+        if response.headers.get("Content-Type") == "application/json":
+            exception_detail = response.json()["detail"]
+        else:
+            exception_detail = response.text
         raise BugoutResponseException(
             "An exception occurred at Bugout API side",
-            status_code=r.status_code,
-            detail=exception_detail["detail"]
-            if exception_detail["detail"] is not None
-            else None,
+            status_code=response.status_code,
+            detail=exception_detail,
         )
     except Exception as e:
-        raise BugoutUnexpectedResponse(f"{str(e)}")
-    return response_body
+        raise BugoutUnexpectedResponse({str(e)})
+    return response.json()
 
 
 def ping(url: str) -> Dict[str, Any]:
